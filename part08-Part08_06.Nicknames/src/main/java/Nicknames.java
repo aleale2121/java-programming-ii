256f8bbfe0a710fd17980f45f111ce85f3d69d1f
alefew <alefewyimer4@gmail.com>
2024-04-25 23:38:48 +0300

    Nicknames

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -5,6 +5,11 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+        HashMap<String,String> nicknames=new HashMap<>();
+        nicknames.put("matthew's","matt");
+        nicknames.put("michael's","mix");
+        nicknames.put("arthur's","artie");
+
     }
 
 }
