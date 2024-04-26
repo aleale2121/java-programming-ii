faa0ed16507b8b786efedf04b20d8fc613617992
alefew <alefewyimer4@gmail.com>
2024-04-26 23:01:57 +0300

    $last_part

---
+++ part09-Part09_01.ABC/src/main/java/C.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alefew
+ */
+public class C extends B {
+
+    public C() {
+        super();
+    }
+
+    public void c() {
+        System.out.println("C");
+    }
+}
