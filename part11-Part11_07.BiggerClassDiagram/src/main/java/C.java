f924024d08aa873941118df522b8f5e897a3c6d1
alefew <alefewyimer4@gmail.com>
2024-04-28 23:10:34 +0300

    BiggerClassDiagram

---
+++ part11-Part11_07.BiggerClassDiagram/src/main/java/C.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author alefew
+ */
+public class C extends B implements IC {
+
+    private List<E> e;
+
+    public C() {
+        e = new ArrayList<>();
+    }
+}
---
+++ part11-Part11_07.BiggerClassDiagram/src/main/java/E.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author alefew
+ */
+public class E {
+
+    private List<C> c;
+
+    public E() {
+        c = new ArrayList<>();
+    }
+}
