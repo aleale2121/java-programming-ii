32f5f637ad68ea159d94966af127263964f4041c
alefew <alefewyimer4@gmail.com>
2024-04-28 23:10:34 +0300

    ShowAndTicket

---
+++ part11-Part11_03.ShowAndTicket/src/main/java/Show.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author alefew
+ */
+public class Show {
+
+    private String movie;
+    private String time;
+//    private List<Ticket> tickets;
+
+    public Show(String movie, String time) {
+        this.movie = movie;
+        this.time = time;
+//        this.tickets = new ArrayList<>();
+    }
+
+}
