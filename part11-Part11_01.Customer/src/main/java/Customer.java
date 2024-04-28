cfac3e40205755658971034f9ec9311cd2c3ca7c
alefew <alefewyimer4@gmail.com>
2024-04-28 23:10:34 +0300

    Customer

---
+++ part11-Part11_01.Customer/src/main/java/Customer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alefew
+ */
+public class Customer {
+
+    public Customer(String name, String address, String email) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+
+    private String name;
+    private String address;
+    private String email;
+}
